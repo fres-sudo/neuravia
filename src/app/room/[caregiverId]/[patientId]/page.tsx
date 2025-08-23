f11513b0c4db2c162970c45b0ad30b72b32a57cd
Glauco Rampone <glaucorampone@gmail.com>
2025-08-24 00:40:10 +0200

    Game improvements: skip profession request for existing patients, add back button to statistics page, update diary warning message

--- src/app/room/[caregiverId]/[patientId]/page.tsx
+++ src/app/room/[caregiverId]/[patientId]/page.tsx
@@ -1,21 +1,21 @@
 "use client";
 import React from "react";
-// import { useParams } from "next/navigation";
+import { useParams } from "next/navigation";
 import { useGameStore } from "@/stores/game-store";
 import { WelcomeScreen } from "@/components/game/screens/welcome-screen";
 import { GameScreen } from "@/components/game/screens/game-screen";
 
 function RoomPage() {
-	// const params = useParams();
-	// const caregiverId = params.caregiverId as string;
-	// const patientId = params.patientId as string;
+	const params = useParams();
+	const caregiverId = params.caregiverId as string;
+	const patientId = params.patientId as string;
 
 	const showWelcome = useGameStore((state) => state.showWelcome);
 	const isPlaying = useGameStore((state) => state.isPlaying);
 
 	return (
 		<div className="font-sans bg-white">
-			{showWelcome && <WelcomeScreen />}
+			{showWelcome && <WelcomeScreen patientId={patientId} />}
 			{isPlaying && <GameScreen />}
 		</div>
 	);
